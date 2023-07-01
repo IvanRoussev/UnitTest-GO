b3ee8ba99a1fe12d2c699e05c3c5e1fddcce6c25
Ivan Roussev <ivan.roussev12@gmail.com>
2023-07-01 16:16:18 -0700

    implemented table driven tests

--- calculator_test.go
+++ calculator_test.go
@@ -1,43 +1,69 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestDiscountApplied(t *testing.T) {
+func TestDiscountCalculator(t *testing.T) {
+	type testCase struct {
+		minimumPurchaseAmount int
+		discount int
+		purchaseAmount int
+		expectedAmountint int	
+	}
 
-	calculator := NewDiscountCalculator(100, 20)
-	amount := calculator.Calculate(150)
+	testCases := []testCase{
+		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 150, expectedAmountint: 130},
+		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 200, expectedAmountint: 160},
+		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 350, expectedAmountint: 290},
+		{minimumPurchaseAmount: 100, discount: 20, purchaseAmount: 50, expectedAmountint: 50},
+	}
+	for _, tc := range testCases {
+		calculator := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discount)
+		amount := calculator.Calculate(tc.purchaseAmount)
 
-	if amount != 130 {
+	if amount != tc.expectedAmountint {
 		t.Errorf("Expected 130, Got %v", amount)
 	}
-}
-
-func TestDiscountMultipliedByTwoApplied(t *testing.T) {
-
-	calculator := NewDiscountCalculator(100, 20)
-	amount := calculator.Calculate(200)
-
-	if amount != 160 {
-		t.Errorf("Expected 160, Got %v", amount)
 	}
 }
 
-func TestDiscountMultipliedByThreeApplied(t *testing.T) {
+// func TestDiscountApplied(t *testing.T) {
 
-	calculator := NewDiscountCalculator(100, 20)
-	amount := calculator.Calculate(350)
+// 	calculator := NewDiscountCalculator(100, 20)
+// 	amount := calculator.Calculate(150)
 
-	if amount != 290 {
-		t.Errorf("Expected 290, Got %v", amount)
-	}
-}
+// 	if amount != 130 {
+// 		t.Errorf("Expected 130, Got %v", amount)
+// 	}
+// }
 
-func TestDiscountNotApplied(t *testing.T) {
+// func TestDiscountMultipliedByTwoApplied(t *testing.T) {
 
-	calculator := NewDiscountCalculator(100, 20)
-	amount := calculator.Calculate(60)
+// 	calculator := NewDiscountCalculator(100, 20)
+// 	amount := calculator.Calculate(200)
 
-	if amount != 60 {
-		t.Errorf("Expected 50, got %v. Failed because the discount was not expected to be applied", amount)
-	}
-} 
+// 	if amount != 160 {
+// 		t.Errorf("Expected 160, Got %v", amount)
+// 	}
+// }
+
+// func TestDiscountMultipliedByThreeApplied(t *testing.T) {
+
+// 	calculator := NewDiscountCalculator(100, 20)
+// 	amount := calculator.Calculate(350)
+
+// 	if amount != 290 {
+// 		t.Errorf("Expected 290, Got %v", amount)
+// 	}
+// }
+
+// func TestDiscountNotApplied(t *testing.T) {
+
+// 	calculator := NewDiscountCalculator(100, 20)
+// 	amount := calculator.Calculate(60)
+
+// 	if amount != 60 {
+// 		t.Errorf("Expected 50, got %v. Failed because the discount was not expected to be applied", amount)
+// 	}
+// } 
